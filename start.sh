ff80919274c3ad6d3dd2057ca4a28b6966721c50
Energy1190 <energyneo0@gmail.com>
2017-07-25 14:11:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 
 NODELIST=$@
 
-sed '/^.*server.*:3306.*$/d' /usr/local/etc/hroxy/haproxy.cfg | sed '/^$/d' >> /haproxy.cfg
+sed '/^.*server.*:3306.*$/d' /usr/local/etc/haproxy/haproxy.cfg | sed '/^$/d' >> /haproxy.cfg
+cp /haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 IFS=' ' read -ra RESULT <<< ${NODELIST} 
 for i in ${#RESULT[@]}; do
   IFS='---' read -ra PAIR <<< $i
   IFS='/' read -ra NAME <<< ${PAIR[0]}
-  echo "    server ${NAME[-1]} ${PAIR[-1]}:3306" >> /usr/local/etc/hroxy/haproxy.cfg
+  echo "    server ${NAME[-1]} ${PAIR[-1]}:3306" >> /usr/local/etc/haproxy/haproxy.cfg
 done
 
 /docker-entrypoint.sh
