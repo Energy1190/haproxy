e6d656826809447e492d7cde8933beaf27239599
Energy1190 <energyneo0@gmail.com>
2017-07-25 16:29:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ IFS=$'\r\n' GLOBIGNORE='*' command eval  'NODELIST=($(cat /etc/nodes))'
 sed '/^.*server.*:3306.*$/d' /usr/local/etc/haproxy/haproxy.cfg | sed '/^$/d' > /haproxy.cfg
 cp /haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 
-for i in ${#NODELIST[@]}; do
-  IFS='---' read -ra PAIR <<< $NODELIST[$i]
+for (( i=0; i<${#NODELIST[@]}; i++ )); do
+  IFS='+' read -ra PAIR <<< ${NODELIST[$i]}
   IFS='/' read -ra NAME <<< ${PAIR[0]}
   echo "    server ${NAME[-1]} ${PAIR[-1]}:3306" >> /usr/local/etc/haproxy/haproxy.cfg
 done
