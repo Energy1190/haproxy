2231113965c9e3fa4dcf10e137866a79bce02387
Energy1190 <energyneo0@gmail.com>
2017-07-27 15:05:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ cp /haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 for (( i=0; i<${#NODELIST[@]}; i++ )); do
   IFS='+' read -ra PAIR <<< ${NODELIST[$i]}
   IFS='/' read -ra NAME <<< ${PAIR[0]}
-  sed 's/balance roundrobin/balance roundrobin\n    server ${NAME[-1]} ${PAIR[-1]}:3306/g' /usr/local/etc/haproxy/haproxy.cfg > /haproxy.cfg
+  sed 's/balance roundrobin/balance roundrobin\n    server $NAME[-1] $PAIR[-1]:3306/g' /usr/local/etc/haproxy/haproxy.cfg > /haproxy.cfg
   cp /haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
   echo "    server ${NAME[-1]} ${PAIR[-1]}:3306" >> /usr/local/etc/haproxy/haproxy.cfg
 done
