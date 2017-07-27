5a4c9956983cb70e66cb538f74a3299c018d4ca5
Energy1190 <energyneo0@gmail.com>
2017-07-27 15:14:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,9 +8,11 @@ cp /haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 for (( i=0; i<${#NODELIST[@]}; i++ )); do
   IFS='+' read -ra PAIR <<< ${NODELIST[$i]}
   IFS='/' read -ra NAME <<< ${PAIR[0]}
-  sed 's/balance roundrobin/balance roundrobin\n    server $NAME[-1] $PAIR[-1]:3306/g' /usr/local/etc/haproxy/haproxy.cfg > /haproxy.cfg
+  N=${NAME[-1]}
+  P=${$PAIR[-1]}
+  sed 's/balance roundrobin/balance roundrobin\n    server $N $P:3306/g' /usr/local/etc/haproxy/haproxy.cfg > /haproxy.cfg
   cp /haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
-  echo "    server ${NAME[-1]} ${PAIR[-1]}:3306" >> /usr/local/etc/haproxy/haproxy.cfg
+  echo "    server ${N} ${P}:3306" >> /usr/local/etc/haproxy/haproxy.cfg
 done
 
 /docker-entrypoint.sh "$@"
